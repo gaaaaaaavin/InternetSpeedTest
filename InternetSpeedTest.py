589cf5bd26b2ab9517af43f1792dd439aee318b4
gaaaaaaavin <59616705+gaaaaaaavin@users.noreply.github.com>
2022-05-16 19:33:25 +0530

    recheck every 60 seconds if connection not available

--- InternetSpeedTest.py
+++ InternetSpeedTest.py
@@ -6,12 +6,15 @@ time.sleep(10)	# 10 seconds before speed test only for startup using crontab
 
 logging.basicConfig(format="%(asctime)s:%(levelname)s:%(message)s",filename="internetchecker.log",level=logging.DEBUG)
 st=speedtest.Speedtest()
+x=True
 
-try:
-	downSpeed=str(int(st.download()*pow(10,-6)))
-	upSpeed=str(int(st.upload()*pow(10,-6)))
-	speedLog="Download: "+downSpeed+"MB/s\t||\tUpload: "+upSpeed+"MB/s"
-	logging.info(speedLog)
-except:
-	logging.error("no connection")
-
+while x==True:
+	try:
+		downSpeed=str(int(st.download()*pow(10,-6)))
+		upSpeed=str(int(st.upload()*pow(10,-6)))
+		speedLog="Download: "+downSpeed+"MB/s\t||\tUpload: "+upSpeed+"MB/s"
+		logging.info(speedLog)
+		x=False
+	except:
+		logging.error("no connection")
+		time.sleep(60)
