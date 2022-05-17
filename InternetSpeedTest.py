30997a6b5f9f74d48dc668307029456e1eec03ac
gaaaaaaavin <59616705+gaaaaaaavin@users.noreply.github.com>
2022-05-17 09:05:44 +0530

    added safely with use of crontab
    since the code runs in an interval of 15 minutes and the previous changes looped the code till an internet connection was established before ending, i added a safety feature which automatically ends the code after a couple of tried. this will allow the code to end and crontab will run the code again without crossing over.

--- InternetSpeedTest.py
+++ InternetSpeedTest.py
@@ -6,15 +6,17 @@ time.sleep(10)	# 10 seconds before speed test only for startup using crontab
 
 logging.basicConfig(format="%(asctime)s:%(levelname)s:%(message)s",filename="internetchecker.log",level=logging.DEBUG)
 st=speedtest.Speedtest()
-x=True
+x=0
 
-while x==True:
+while x<13:
 	try:
 		downSpeed=str(int(st.download()*pow(10,-6)))
 		upSpeed=str(int(st.upload()*pow(10,-6)))
 		speedLog="Download: "+downSpeed+"MB/s\t||\tUpload: "+upSpeed+"MB/s"
 		logging.info(speedLog)
-		x=False
+		x=13
 	except:
 		logging.error("no connection")
+		x=x+1
 		time.sleep(60)
+
